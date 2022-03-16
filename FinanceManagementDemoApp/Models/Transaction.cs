0c86be64df97e67e1de8dff45b57c55dd951039d
wildfirefly12 <wildfirefly12@gmail.com>
2022-03-16 17:52:46 -0600

    Init commit

---
+++ FinanceManagementDemoApp/Models/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FinanceManagementDemoApp.Models
+{
+    public class Transaction
+    {
+        public long Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public double Amount { get; set; }
+        public long AccountId { get; set; }
+        public Account Account { get; set; }
+        public long CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
